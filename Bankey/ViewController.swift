3850318eb9feb3d94d8592eb4e8d43efeeee60eb
PeriwinkleFTW <29137960+periwinkleFTW@users.noreply.github.com>
2023-07-31 21:53:11 -0600

    Initial Commit

---
+++ Bankey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Andrey Frol on 2023-07-31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
