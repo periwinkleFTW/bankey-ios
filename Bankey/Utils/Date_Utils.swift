9a7a658a3fe30aabca63666c7935e6f0bc91b05e
PeriwinkleFTW <29137960+periwinkleFTW@users.noreply.github.com>
2023-08-29 15:57:41 -0600

    network calls

---
+++ Bankey/Utils/Date_Utils.swift
@@ -0,0 +1,22 @@
+//
+//  Date_Utils.swift
+//  Bankey
+//
+//  Created by Andrey Frol on 2023-08-29.
+//
+
+import Foundation
+
+extension Date {
+    static var bankeyDateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone(abbreviation: "MDT")
+        return formatter
+    }
+    
+    var monthDayYearString: String {
+        let dateFormatter = Date.bankeyDateFormatter
+        dateFormatter.dateFormat = "MMM d, yyyy"
+        return dateFormatter.string(from: self)
+    }
+}
